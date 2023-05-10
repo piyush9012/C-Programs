e5ef3c6a793ca880f9d216838f794602f929924d
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-05-10 17:46:08 +0530

    Between_prime.cpp

--- betweenprime.cpp
+++ betweenprime.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-	int a, b, f=0;
-	cout<<"Enter two numbers"<<endl;
+	int a, b;
+	cout<<"Enter the range of numbers"<<endl;
 	cin>>a>>b;
 	cout<<"In between numbers are"<<endl;
-	while(a<b)
+	for(int num=a; num<=b; num++)
 	{
-		f=0;
-		if(a==0 || b==0) f=1;
-		for(int i=2; i<=a/2; i++)
+		int i;
+		for (i=2; i<num; i++)
 		{
-			if(a%i==0)
+			if (num%i == 0)
 			{
-				f=1;
 				break;
 			}
 		}
-		if(f==0) cout<<a<<" ";
-		++a;
+		if (i == num)
+		{
+			cout<<num<<" ";
+		}
 	}
 	return 0;
 }
